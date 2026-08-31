6c3888fc2e879cb8398b9326ebae3af47508e506
Charulatha R <rcharulatha2006@gmail.com>
2026-08-31 22:25:04 +0530

    Delete case study 15 directory

--- case study 15/script.js
+++
@@ -1,7 +0,0 @@
-function calculateVolume() {
-	let radius = Number(document.getElementById("radius").value);
-
-	let volume = (4 / 3) * Math.PI * radius * radius * radius;
-
-	document.getElementById("volume").value = volume.toFixed(2);
-}
